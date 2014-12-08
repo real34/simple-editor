ada23981a0c0442135b2209b80bae26a32a9517b
Pierre Martin <pierre@occitech.fr>
2014-12-08 14:20:53 +0100

    WIP Flux

--- js/app.js
+++ js/app.js
@@ -1,33 +1,90 @@
-var React = require('react');
-var Reflux = require('reflux');
 var markdown = require('markdown').markdown;
+var React = require('react');
+var AppDispatcher = require('flux').Dispatcher;
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
 
-var EditorActions = Reflux.createActions(['contentChanged', 'fileLoaded']);
+var EditorConstants = {
+	CONTENT_CHANGED: 'CONTENT_CHANGED',
+	FILE_LOADED: 'FILE_LOADED'
+};
+var EditorActions = {
+	changeContent: function(newContent) {
+		AppDispatcher.dispatch({
+			actionType: EditorConstants.CONTENT_CHANGED,
+			content: newContent
+		});
+	},
+	loadFile: function(filename, newContent) {
+		AppDispatcher.dispatch({
+			actionType: EditorConstants.FILE_LOADED,
+			filename: filename,
+			content: newContent
+		});
+	}
+};
 
-var ContentStore = (function() {
-	var _currentContent = '';
-	return Reflux.createStore({
-		listenables: EditorActions,
-		onContentChanged: function(newContent) {
-			_currentContent = newContent;
-			this.trigger(_currentContent);
-		},
-		onFileLoaded: function(fileInformation) {
-			_currentContent = fileInformation.content;
-			this.trigger(_currentContent);
-		},
-		getInitialState: function() {
-			return ["#Hello world!", "Welcome to **markdown**"].join("\n\n");
+function ContentStore() {
+	var self = this;
+	this._currentContent = ["#Hello world!", "Welcome to **markdown**"].join("\n\n");
+	AppDispatcher.register(function(payload) {
+		var action = payload.actionType;
+
+		switch (action.actionType) {
+			case EditorConstants.CONTENT_CHANGED:
+			case EditorConstants.FILE_LOADED:
+				self.updateContent(content);
+			break;
 		}
 	});
-})();
+}
+util.inherits(ContentStore, EventEmitter);
+ContentStore.prototype.getCurrentContent = function() {
+	return this._currentContent;
+};
+ContentStore.prototype.updateContent = function(newContent) {
+	this._currentContent = newContent;
+	this.emit('change');
+};
+
+var ContentStore = new ContentStore();
+
+// var ContentStore = (function() {
+// 	var _currentContent = '';
+// 	return Reflux.createStore({
+// 		listenables: EditorActions,
+// 		onContentChanged: function(newContent) {
+// 			_currentContent = newContent;
+// 			this.trigger(_currentContent);
+// 		},
+// 		onFileLoaded: function(fileInformation) {
+// 			_currentContent = fileInformation.content;
+// 			this.trigger(_currentContent);
+// 		},
+// 		getInitialState: function() {
+// 			return ["#Hello world!", "Welcome to **markdown**"].join("\n\n");
+// 		}
+// 	});
+// })();
 
 var SimpleEditor = React.createClass({
+	_stateFromStore: function() {
+		return { value: ContentStore.getCurrentContent() }
+	},
 	getInitialState: function() {
-		return { value: ContentStore.getInitialState() };
+		return this._stateFromStore();
+	},
+	_onChange: function() {
+		this.setState(this._stateFromStore());
+	},
+	componentDidMount: function() {
+		ContentStore.on('change', this._onChange);
+	},
+	componentWillUnmount: function() {
+		ContentStore.removeListener('change', this._onChange);
 	},
 	handleChange: function(event) {
-		EditorActions.contentChanged(event.target.value);
+		EditorActions.changeContent(event.target.value);
 	},
 	render: function() {
 		return (
@@ -40,7 +97,7 @@ var SimpleEditor = React.createClass({
 });
 
 var SimplePreview = React.createClass({
-	mixins: [Reflux.listenTo(ContentStore, 'onContentChanged', 'onContentChanged')],
+	// mixins: [Reflux.listenTo(ContentStore, 'onContentChanged', 'onContentChanged')],
 	getInitialState: function() {
 		return { html : '' };
 	},
@@ -63,7 +120,7 @@ var SimplePreview = React.createClass({
 });
 
 var FileManager = React.createClass({
-	mixins: [Reflux.listenTo(ContentStore, 'onContentChanged', 'onContentChanged')],
+	// mixins: [Reflux.listenTo(ContentStore, 'onContentChanged', 'onContentChanged')],
 	handleUpload: function(event) {
 		var reader = new FileReader();
 		var file = event.target.files[0];
