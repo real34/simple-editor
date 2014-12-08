d984a30350b88c99423336f89ba46472eb7c9e6d
Pierre Martin <pierre@occitech.fr>
2014-12-08 13:44:04 +0100

    Added file management

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ var React = require('react');
 var Reflux = require('reflux');
 var markdown = require('markdown').markdown;
 
-var EditorActions = Reflux.createActions(['contentChanged']);
+var EditorActions = Reflux.createActions(['contentChanged', 'fileLoaded']);
 
 var ContentStore = (function() {
 	var _currentContent = '';
@@ -12,25 +12,15 @@ var ContentStore = (function() {
 			_currentContent = newContent;
 			this.trigger(_currentContent);
 		},
+		onFileLoaded: function(fileInformation) {
+			_currentContent = fileInformation.content;
+			this.trigger(_currentContent);
+		},
 		getInitialState: function() {
 			return ["#Hello world!", "Welcome to **markdown**"].join("\n\n");
 		}
 	});
 })();
-
-var ContentHistoryStore = Reflux.createStore({
-	init: function() {
-		this.history = [];
-		this.listenTo(ContentStore, this.onContentChanged, this.onContentChanged);
-	},
-	onContentChanged: function(newContent) {
-		this.history.push({
-			date: new Date(),
-			content: newContent
-		});
-		this.trigger(this.history);
-	}
-});
 
 var SimpleEditor = React.createClass({
 	getInitialState: function() {
@@ -72,10 +62,47 @@ var SimplePreview = React.createClass({
 	}
 });
 
+var FileManager = React.createClass({
+	mixins: [Reflux.listenTo(ContentStore, 'onContentChanged', 'onContentChanged')],
+	handleUpload: function(event) {
+		var reader = new FileReader();
+		var file = event.target.files[0];
+
+		reader.onload = function() {
+			EditorActions.fileLoaded({
+				name: file.name,
+				content: this.result
+			});
+		};
+		reader.readAsText(file);
+	},
+	handleDownload: function(event) {
+		// For a wider range of compatibility, see https://github.com/eligrey/FileSaver.js
+		var blob = new Blob([this.state.markdown], {
+			type: "text/markdown;charset=utf-8;"
+		});
+		var fileURL = window.URL.createObjectURL(blob); // window.URL.revokeObjectURL(fileURL);
+		event.target.setAttribute('download', window.prompt('Please enter the filename', 'simple-text.md'));
+		event.target.setAttribute('href', fileURL);
+	},
+	onContentChanged: function(newContent) {
+		this.setState({ markdown: newContent });
+	},
+	render: function() {
+		return (
+			<ul>
+				<li><input type="file" name="file-uploader" onChange={ this.handleUpload } /></li>
+				<li><a href="" download="" onClick={ this.handleDownload }>Download Mardown file</a></li>
+			</ul>
+		);
+	}
+});
+
 var SimpleEditorWindow = React.createClass({
 	render: function() {
 		return (
 			<div>
+				<FileManager />
 				<SimpleEditor />
 				<SimplePreview />
 			</div>
