0b1b56407f37dff10172bbf6b6072a9fba7d7baa
Pierre Martin <contact@pierre-martin.fr>
2014-12-09 00:10:08 +0100

    Quick and dirty debug bar

--- js/app.js
+++ js/app.js
@@ -18,12 +18,42 @@ var ContentStore = createStore({
 	},
 	getCurrentContent: function() {
 		return this.currentContent;
+	},
+	dehydrate: function() {
+		return { currentContent: this.currentContent }
+	},
+	rehydrate: function(state) {
+		this.currentContent = state.currentContent;
+		this.emitChange();
+	}
+});
+
+var EventStore = createStore({
+	storeName: 'EventStore',
+	initialize: function() {
+		this.events = [];
+	},
+	handlers: {
+		'default': 'trackEvent'
+	},
+	trackEvent: function(payload, eventName) {
+		this.events.push({
+			name: eventName,
+			payload: payload
+		});
+		this.emitChange();
+	},
+	getEvents: function() {
+		return this.events;
 	}
 });
 
 Dispatchr.registerStore(ContentStore);
+Dispatchr.registerStore(EventStore);
 
-var dispatcher = new Dispatchr();
+var context = {};
+var dispatcher = new Dispatchr(context);
+
 var EditorActions = {
 	changeContent: function(newContent) {
 		dispatcher.dispatch('CONTENT_CHANGED', {
@@ -38,20 +68,24 @@ var EditorActions = {
 	}
 };
 
-var ContentStoreListenerMixin = {
-	componentDidMount: function() {
-		dispatcher.getStore('ContentStore').addChangeListener(this._onChange);
-	},
-	componentWillUnmount: function() {
-		dispatcher.getStore('ContentStore').removeChangeListener(this._onChange);
-	},
-	_onChange: function() {
-		this.setState(this.getInitialState());
-	},
-	getInitialState: function() {
-		return this._stateFromStore(dispatcher.getStore('ContentStore'));
-	},
+var StoreListenerMixin = function(storeName) {
+	return {
+		componentDidMount: function() {
+			dispatcher.getStore(storeName).addChangeListener(this._onChange);
+		},
+		componentWillUnmount: function() {
+			dispatcher.getStore(storeName).removeChangeListener(this._onChange);
+		},
+		_onChange: function() {
+			this.setState(this.getInitialState());
+		},
+		getInitialState: function() {
+			return this._stateFromStore(dispatcher.getStore(storeName));
+		},
+	}
 };
+var ContentStoreListenerMixin = StoreListenerMixin('ContentStore');
+var EventStoreListenerMixin = StoreListenerMixin('EventStore');
 
 var SimpleEditor = React.createClass({
 	mixins: [ContentStoreListenerMixin],
@@ -124,10 +158,93 @@ var FileManager = React.createClass({
 	}
 });
 
+var DebugBar = React.createClass({
+	mixins: [EventStoreListenerMixin],
+	_stateFromStore: function(store) {
+		return { eventCount: store.getEvents().length }
+	},
+	componentDidMount: function() {
+		this.initialState = dispatcher.dehydrate();
+	},
+
+	handleDownloadState: function() {
+		var currentAppState = JSON.stringify(dispatcher.dehydrate());
+		var fileURL = window.URL.createObjectURL(new Blob(
+			[currentAppState],
+			{ type: "text/json;charset=utf-8;"}
+		));
+		var nowSuffix = new Date(Date.now()).toLocaleString();
+		event.target.setAttribute('download', 'simple-editor-state-' + nowSuffix);
+		event.target.setAttribute('href', fileURL);
+	},
+	handleUploadState: function(event) {
+		var reader = new FileReader();
+		reader.onload = function() {
+			dispatcher.rehydrate(JSON.parse(this.result));
+		};
+		reader.readAsText(event.target.files[0]);
+	},
+
+	handleDownloadHistory: function() {
+		var currentAppHistory = {
+			initialState: this.initialState,
+			events: dispatcher.getStore('EventStore').getEvents()
+		};
+		var fileURL = window.URL.createObjectURL(new Blob(
+			[JSON.stringify(currentAppHistory)],
+			{ type: "text/json;charset=utf-8;"}
+		));
+		var nowSuffix = new Date(Date.now()).toLocaleString();
+		event.target.setAttribute('download', 'simple-editor-history-' + nowSuffix);
+		event.target.setAttribute('href', fileURL);
+	},
+	handleUploadHistory: function(event) {
+		var reader = new FileReader();
+		reader.onload = function() {
+			var HISTORY_SPEED_IN_MS = 100;
+			var history = JSON.parse(this.result);
+			console.debug('rebuilding interface from history...');
+			console.debug('... back to initial state');
+			dispatcher.rehydrate(history.initialState);
+
+			window.setTimeout(function applyNextEvent() {
+				var eventToApply = history.events.shift();
+				console.debug('... applying next event - left: ' + history.events.length);
+				dispatcher.dispatch(eventToApply.name, eventToApply.payload);
+
+				if (history.events.length) {
+					window.setTimeout(applyNextEvent, HISTORY_SPEED_IN_MS);
+				}
+			}, HISTORY_SPEED_IN_MS);
+		};
+		reader.readAsText(event.target.files[0]);
+	},
+
+	render: function() {
+		return (
+			<div>
+				<p>
+					<strong>DEBUG BAR</strong>
+					- {this.state.eventCount} events tracked
+				</p>
+				<p>
+					<input type="file" onChange={ this.handleUploadState } />
+					- <a href="" download="" onClick={ this.handleDownloadState }>Download current state</a>
+				</p>
+				<p>
+					<input type="file" onChange={ this.handleUploadHistory } />
+					- <a href="" download="" onClick={ this.handleDownloadHistory }>Download actions history</a>
+				</p>
+			</div>
+		);
+	}
+});
+
 var SimpleEditorWindow = React.createClass({
 	render: function() {
 		return (
 			<div>
+				<DebugBar />
 				<FileManager />
 				<SimpleEditor />
 				<SimplePreview />
